c334d8e314b0f8ab2e094febafa2eb5288a9fe82
cph-ml444 <31694580+cph-ml444@users.noreply.github.com>
2018-01-16 00:10:04 +0100

    Add files via upload

--- login.php
+++ login.php
@@ -14,7 +14,7 @@
 
 	body {
 		background-image: url(img/airport2.jpg);
-		background-size: cover;
+		background-size: 100%;
 		background-repeat: no-repeat;
 		background-color: #212121;
 	}
@@ -25,6 +25,7 @@
 		background-color: #D1011B;
 	    border: none;
         border-radius: 40px 10px;
+        margin-bottom: 250px;
     }
     form {
 		width: 70%;
@@ -36,11 +37,14 @@
 		height: 40px;
 		padding-top: 5px;
 		padding-bottom: 5px;
+        padding-left: 10px;
 		border: none;
 		background-color: #fff;
 		line-height: 40px;
 		font-size: 16px;
 		margin-bottom: 4px;
+        border-radius: 15px;
+
 			
 	}
 	form button {
@@ -53,6 +57,7 @@
 		cursor: pointer;
 		margin: 0 auto;
 		color: #fff;
+        margin-top: 10px;
 		margin-bottom: 10px;
 			
 	}
@@ -82,18 +87,23 @@
 		text-decoration: none;
 	}
 	@media only screen and (max-width: 780px) {
-	    .wrapper {
-	        width: 250px;
+	    
+        .wrapper {
+	        width: 70%;
+            margin-bottom: 50px;
+            margin-top: 130px;   
+            
 	    }
 	    form {
-	    width: 200px;
+	    width: 200px;  
 		margin: 0 auto;
-		padding-top: 100px;
-		padding-bottom: 100px;
+        margin-top: 30px;    
 	    }
+        
 	    form button {
 	        width: 40%;
 	    }
+    
 	}
 </style>
 
@@ -141,7 +151,7 @@
 <form action="SQL/login_sql.php" method="POST">
 <p>Please Login to explore</p>
 	<input type="text" name="user_uid" placeholder="Username">
-	<input type="text" name="user_pwd" placeholder="Password">
+	<input type="password" name="user_pwd" placeholder="Password (case sensitive)">
 	<button type="submit" name="submit">Login</button> 
 	<a href="register.php"><p>Not yet registered?</p></a>
 </form>
@@ -150,5 +160,91 @@
 
 </div>
 
+  <div class="footer-section">
+           <div class="colums1">
+            <div class="colum">
+            <p>About Emirates</p>
+            	<ul>
+            		<li><a href="https://www.emirates.com/dk/english/about/press-room.aspx">About us</a></li>
+            		<li><a href="https://www.emirates.com/dk/english/about/emirates-sponsorships/sponsorships.aspx">Sponsorships</a></li>
+            		<li><a href="https://www.emirates.com/dk/English/open-skies/issue/4548519/november-2017">Open Skies magazine</a></li>
+           		  <li><a href="http://www.emiratesgroupcareers.com/">Careers</a></li>
+            	</ul>
+            </div>
+            
+            <div class="colum">
+            <p>Top picks</p>
+            	<ul>
+            		<li><a href="https://www.emirates.com/dk/English/destinations/flights-to-dubai.aspx">Flights to Dubai</a></li>
+            		<li><a href="https://www.emirates.com/dk/English/destinations/flights-to-hong-kong.aspx">Flights to Hong Kong</a></li>
+            		<li><a href="https://www.emirates.com/dk/English/destinations/flights-to-singapore.aspx">Flights to Singapore</a></li>
+            		<li><a href="https://www.emirates.com/dk/English/destinations/flights-to-dublin.aspx">Flights to Dublin</a></li>
+            	</ul>
+            </div>
+            
+            <div class="colum">
+            <p>Top countries</p>
+            	<ul>
+            		<li><a href="https://www.emirates.com/dk/English/destinations/flights-to-new-zealand.aspx">Flights to New Zealand</a></li>
+            		<li><a href="https://www.emirates.com/dk/English/destinations/flights-to-australia.aspx">Flights to Australia</a></li>
+            		<li><a href="https://www.emirates.com/dk/English/destinations/flights-to-india.aspx">Flights to India</a></li>
+            		<li><a href="https://www.emirates.com/dk/English/destinations/flights-to-canada.aspx">Flights to Canada</a></li>
+            	</ul>
+            </div>
+             
+            <div class="colum">
+            <p>Top cities</p>
+            	<ul>
+            		<li><a href="https://www.emirates.com/dk/English/destinations/flights-to-perth.aspx">Flights to Perth</a></li>
+            		<li><a href="https://www.emirates.com/dk/English/destinations/flights-to-boston.aspx">Flights to Boston</a></li>
+            		<li><a href="https://www.emirates.com/dk/English/destinations/flights-to-brisbane.aspx">Flights to Brisbane</a></li>
+            		<li><a href="https://www.emirates.com/dk/English/destinations/flights-to-budapest.aspx">Flights to Budapest</a></li>
+            	</ul>
+            </div>
+            </div>
+            
+            <div class="colums2">
+            <div class="colum">
+            <p>Book</p>
+            	<ul>
+            		<li><a href="https://www.emirates.com/dk/english/book/">Book flights</a></li>
+            		<li><a href="https://www.emirates.com/dk/english/travel-services/">Travel services</a></li>
+            		<li><a href="https://www.emirates.com/dk/english/transportation/">Transportation</a></li>
+            		<li><a href="https://www.emirates.com/dk/english/planning-your-trip/">Planning your trip</a></li>
+            	</ul>
+            </div>
+            
+            <div class="colum">
+            <p>Manage</p>
+            	<ul>
+            		<li><a href="https://www.emirates.com/dk/english/manage-your-booking/">Manage your booking</a></li>
+            		<li><a href="https://www.emirates.com/dk/english/baggage/">Baggage</a></li>
+            		<li><a href="https://www.emirates.com/dk/english/travel-updates/">Travel updates</a></li>
+           		  <li><a href="https://www.emirates.com/dk/english/check-in/">Check in</a></li>
+            	</ul>
+            </div>
+            
+            <div class="colum">
+            <p>Experience</p>
+            	<ul>
+            		<li><a href="https://www.emirates.com/dk/english/experience/cabin-features/">Cabin features</a></li>
+            		<li><a href="https://www.emirates.com/dk/english/experience/shop-emirates/">Shop Emirates</a></li>
+            		<li><a href="https://www.emirates.com/dk/english/experience/inflight-entertainment/">Inflight entertainment</a></li>
+            		<li><a href="https://www.emirates.com/dk/english/experience/dining-and-win/">Dining and wine</a></li>
+            	</ul>
+            </div>
+             
+            <div class="colum">
+            <p>Loyalty</p>
+           	  <ul>
+            		<li><a href="https://www.emirates.com/dk/english/skywards/about/partners/our-partners.aspx">Our partners</a></li>
+            		<li><a href="https://www.emirates.com/dk/english/business-rewards/registration.aspx">register your company</a></li>
+            		<li><a href="https://www.emirates.com/dk/english/business-rewards/the-benefits-of-business-rewards.aspx">Business rewards</a></li>
+            		<li><a href="https://www.emirates.com/account/dk/english/login/login.aspx">Your account</a></li>
+            	</ul>
+            </div>
+            </div>
+          </div>  
+    
 </body>
 </html>
