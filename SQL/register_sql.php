ce18f5656f0af0919d2d838f7303009e5f3f9427
cph-ml444 <31694580+cph-ml444@users.noreply.github.com>
2018-01-16 17:54:34 +0100

    Update register_sql.php

--- SQL/register_sql.php
+++ SQL/register_sql.php
@@ -1,8 +1,8 @@
 <?php
 
 $servername = "";
-$username = "root";
-$password = "root";
+$username = "";
+$password = "";
 $dbname = "marcliljeqvist_dk";
 
 // Create connection
@@ -11,55 +11,33 @@ $conn = new mysqli($servername, $username, $password, $dbname);
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 } 
-
-
+    
 if (isset($_POST['submit'])) {
     
-    $user_first = mysqli_real_escape_string($conn, $_POST['user_first']);
-    $user_last = mysqli_real_escape_string($conn, $_POST['user_last']);
-    $user_email = mysqli_real_escape_string($conn, $_POST['user_email']);
-    $user_uid = mysqli_real_escape_string($conn, $_POST['user_uid']);
-    $user_pwd = mysqli_real_escape_string($conn, $_POST['user_pwd']);
-    $skywards = mysqli_real_escape_string($conn, $_POST['skywards']);
+    $user_first = filter_input(INPUT_POST, 'user_first')
+		or die('Missing/illegal un parameter');
+    $user_last = filter_input(INPUT_POST, 'user_last')
+		or die('Missing/illegal un parameter');
+    $user_email = filter_input(INPUT_POST, 'user_email')
+		or die('Missing/illegal un parameter');
+    $user_uid = filter_input(INPUT_POST, 'user_uid')
+		or die('Missing/illegal un parameter');
+    $user_pwd = filter_input(INPUT_POST, 'user_pwd')
+		or die('Missing/illegal un parameter');
+    $skywards = filter_input(INPUT_POST, 'skywards')
+		or die('Missing/illegal un parameter');
     
-    //undersøg tomme felter
+    $user_pwd = password_hash($user_pwd, PASSWORD_DEFAULT);
     
-    if (empty($user_first) || empty($user_last) || empty($user_email) || empty($user_uid) || empty($user_pwd) || empty($skywards)) {
-        
-       header("location: ../register.php?register=empty");
-       exit();
-        
-    } else {
-        if (!preg_match("/^[a-zA-Z]*$/", $user_first) || !preg_match("/^[a-zA-Z]*$/", $user_last)) {
-            header("location: ../register.php?invalid");
-            exit();
-            
-        } else {
-            if (!filter_var($user_email, FILTER_VALIDATE_EMAIL)) {
-                header("location: ,,/register.php?register=empty");
-                exit();
-                
-            } else {
-                $sql = "SELECT * FROM users WHERE user_uid='$user_uid'";
-                $result = mysqli_query($conn, $sql);
-                $resultCheck = mysqli_num_rows($result);
-                
-                if ($resultCheck > 0) {
-                    header("location: ../register.php?register=usertaken");
-                    exit();
-                    
-                } else {
-                    $hashedPwd = password_hash($user_pwd, PASSWORD_DEFAULT);
-                    $sql = "INSERT INTO users (user_first, user_last, user_email, user_uid, user_pwd, skywards) VALUES ('$user_first', '$user_last', '$user_email', '$user_uid', '$hashedPwd', '$skywards');";
-                    mysqli_query($conn, $sql); 
-                     header("location: ../login.php");
-                    exit();
-                }
-            }
-        }
-    }
-    
-}  else {
-        header("location: ../register.php");
-        exit();
+    $sql = 'INSERT INTO users (user_first, user_last, user_email, user_uid, user_pwd, skywards) VALUES (?, ?, ?, ?, ?, ?)';
+	$stmt = $link->prepare($sql);
+	$stmt->bind_param('ssssss', $user_first, $user_last, $user_email, $user_uid, $user_pwd);
+	$stmt->execute();
+	
+	if ($stmt->affected_rows > 0){
+		echo 'user_uid '.$user_uid.' added ';
+	}
+	else {
+		echo 'Could not add the user... Please try with another username.';
+	}
 }
